2d0950e707d1830472781d588045041b891346a1
amarbayat <amarbayat24@gmail.com>
2022-11-06 14:26:20 -0500

    Level 5 - bCrypt Encryption

--- app.js
+++ app.js
@@ -6,81 +6,87 @@ const app = express();
 app.use(express.static("public"));
 
 const ejs = require('ejs');
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 const _ = require('lodash');
-const md5=require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/secretsDB')
 
 const secretSchema = new mongoose.Schema({
-    username:{
-        type:String,
-        required:[true,'Username is required'],
-        unique:true
+    username: {
+        type: String,
+        required: [true, 'Username is required'],
+        unique: true
     },
-    password:{
-        type:String,
-        required:[true,'Password is required'],
-        unique:true
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+        unique: true
     }
 });
 
-const Secret = mongoose.model('Secrets',secretSchema);
+const Secret = mongoose.model('Secrets', secretSchema);
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.render('home.ejs');
 });
 
 app.route('/login')
-.get((req,res)=>{
-    res.render('login.ejs');
-})
-.post((req,res)=>{
-    const user = req.body.username;
-    const pass = md5(req.body.password);
+    .get((req, res) => {
+        res.render('login.ejs');
+    })
+    .post((req, res) => {
+        const user = req.body.username;
+        const pass = req.body.password;
 
-    Secret.findOne({username:user},(err,result)=>{
-        if(!err){
-            if(result.password === pass){
-                res.render('secrets');
-            }else{
-                console.log("Username/Password is incorrect .. please try again");
+        Secret.findOne({
+            username: user
+        }, (err, result) => {
+            if (!err) {
+                bcrypt.compare(pass, result.password, (err,bcryptResult) => {
+                    if(bcryptResult === true){
+                        res.render('secrets');
+                    }
+                });
             }
-        }
-    });
+        });
 
-});
+    });
 
 app.route('/register')
-.get((req,res)=>{
-    res.render('register.ejs');
-})
-.post((req,res)=>{
-    const user = req.body.username;
-    const pass = md5(req.body.password);
+    .get((req, res) => {
+        res.render('register.ejs');
+    })
+    .post((req, res) => {
+        const user = req.body.username;
+        const pass = req.body.password;
+        bcrypt.hash(pass, saltRounds, (err, hash) => {
+            const userCreate = new Secret({
+                username: user,
+                password: hash
+            });
 
-    const userCreate = new Secret({
-        username:user,
-        password:pass
+            userCreate.save((err) => {
+                if (!err) {
+                    res.redirect('login');
+                } else {
+                    res.redirect('register');
+                }
+            });
+        });
+
     });
 
-    userCreate.save((err)=>{
-        if(!err){
-            res.redirect('login');
-        }
-        else{
-            res.redirect('register');
-        }
-    });
 
-});
-
-
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 });
